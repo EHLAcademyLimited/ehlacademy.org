09a261361242cd2c6a05ec0488816b4f6e23fc97
cliff <cliff.wong@ehlacademy.org>
2022-06-02 14:56:15 +0800

    merge dv

--- src/lib/promotion/RedeemSuccessDialog.svelte
+++ src/lib/promotion/RedeemSuccessDialog.svelte
@@ -5,7 +5,7 @@
 
 <div class="p-4 text-center">
 	<p class="text-3xl mb-4">成功</p>
-	<p>已經成功再延長至7日</p>
+	<p>已延長至7天，學生請登入學生帳號進行學習</p>
 </div>
 
 <button on:click={closeModal} class="button w-full">確定</button>
