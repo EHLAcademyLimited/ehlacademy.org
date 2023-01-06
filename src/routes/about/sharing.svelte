1ea717b17d67a8dd09dfb7ece999edabb77a2c31
cliff <cliff.wong@ehlacademy.org>
2023-01-06 12:41:59 +0800

    cn version

--- src/routes/about/sharing.svelte
+++ src/routes/about/sharing.svelte
@@ -4,65 +4,84 @@
 		{
 			id: 'goFC8EFy6go',
 			title_hk: '新世紀決定電子教育成功的三個維度',
+			title_cn: '新世纪决定电子教育成功的三个维度',
 			author_hk: 'John Furlong教授，前牛津大學教育學院院長',
 			title_en: 'Three Dimensions That Contribute to the Success of e-Education',
-			author_en: 'Professor John Furlong, ex-head of Education Department, Oxford University'
+			author_en: 'Professor John Furlong, ex-head of Education Department, Oxford University',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/John%20Furlong_%20Three%20Dimensions%20That%20Contribute%20to%20the%20Success%20of%20e-Education.mp4\n"
 		},
 		{
 			id: 'FxsoVMA6v78',
 			title_hk: '全球視角下的中小學電子教育的實踐、趨勢與挑戰',
+			title_cn: '全球视角下的中小学电子教育的实践、趋势与挑战',
 			author_hk: 'Niall Winters, 牛津大學教育學院理學碩士教育（學習與技術）課程聯席總監, 牛津大學教育學院學習及新技術學系副教授',
 			title_en: 'The Current Practices, Trends and Challenges in K12 E-learning over the World',
 			author_en: 'Niall Winters, Co-Director of the MSc Education, Associate Professor of Learning and New Technologies, Department of Education, University of Oxford',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/Niall%20Winters_%20The%20Current%20Practices%2C%20Trends%20and%20Challenges%20in%20K12%20E-learning%20over%20the%20World.mp4\n"
 		},
 		{
 			id: '7ztKjnaBNek',
 			title_hk: '如何確保EHLA英文教學資源的一致性、可靠性與權威性',
+			title_cn: '如何确保EHLA英语教学资源的一致性、可靠性与权力',
 			author_hk: 'Adrian Bullock',
 			title_en: 'Managing the Quality of EHLA English Language Teaching and Learning Resources',
-			author_en: 'Adrian Bullock'
+			author_en: 'Adrian Bullock',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/Managing%20the%20Quality%20of%20EHLA%20English%20Language%20Teaching%20and%20Learning%20Resources.mp4"
 		},
 		{
 			id: 'QTJdWATpCLU',
 			title_hk: '總編輯的訪問',
+			title_cn: '总编辑的访问',
 			author_hk: 'EHLA官方頻道',
 			title_en: 'Interview with chief editor',
-			author_en: 'EHLA Official Channel'
+			author_en: 'EHLA Official Channel',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/Interview.mp4"
 		},
 		{
 			id: 'ADcpwIAI17g',
 			title_hk: '閱讀對學習英文的重要性被嚴重低估',
+			title_cn: '阅读对学习英语的重要性被严重低估',
 			author_hk: 'EHLA官方頻道',
 			title_en: 'How Important is Reading for English Learning',
-			author_en: 'EHLA Official Channel'
+			author_en: 'EHLA Official Channel',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/How%20Important%20is%20Reading%20for%20English%20Learning.mp4"
 		},
 		{
 			id: 'qnA3w0PFofI',
 			title_hk: 'EHLA智能教育，一個革命性的教育項目',
+			title_cn: 'EHLA智能教育，一个革新性的教育项目',
 			author_hk: 'EHLA官方頻道',
 			title_en: 'EHLA iEducation, A revolutionary educational project\n',
-			author_en: 'EHLA Official Channel'
+			author_en: 'EHLA Official Channel',
+			video_url: "",
+			is_cn: false
 		},
 		{
 			id: '0TSKToBdBS0',
 			title_hk: '為什麼亞洲學生需要學習英語文學呢？',
+			title_cn: '为什么亚洲学生需要学习英语文学呢？',
 			author_hk: 'EHLA官方頻道',
 			title_en: 'Why is it crucial for students in Asia to engage more deeply in literature?',
-			author_en: 'EHLA Official Channel'
+			author_en: 'EHLA Official Channel',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/Why%20is%20it%20crucial%20for%20students%20in%20Asia%20to%20engage%20more%20deeply%20in%20literature_.mp4"
 		},
 		{
 			id: 's4p6SZ92-Hw',
 			title_hk: 'EHLA智慧教育生態系統與亞洲英語學習環境的聯繫',
+			title_cn: 'EHLA智慧教育生态系统与亚洲英语学习环境的联系',
 			author_hk: 'EHLA官方頻道',
 			title_en: 'The connections between our ecosystem and already-emplaced English learning in Asia.',
-			author_en: 'EHLA官方頻道'
+			author_en: 'EHLA官方頻道',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/The%20connections%20between%20our%20ecosystem%20and%20already-emplaced%20English%20learning%20in%20Asia..mp4"
 		},
 		{
 			id: 'Z96vh_gLx9Q',
 			title_hk: '學習英語文學讓學生的英語能力更上一層樓',
+			title_cn: '学习英语文学让学生的英语能力更上一层楼',
 			author_hk: 'EHLA官方頻道',
 			title_en: 'Providing a diverse range of reading resources that students are sure to grow from.',
-			author_en: 'EHLA Official Channel'
+			author_en: 'EHLA Official Channel',
+			video_url: "https://ehla-media.oss-cn-hongkong.aliyuncs.com/Providing%20a%20diverse%20range%20of%20reading%20resources%20that%20students%20are%20sure%20to%20grow%20from..mp4"
 		}
 	]
 </script>
@@ -74,13 +93,18 @@
 <div class="max-w-screen-lg mx-auto px-4 my-8 sm:my-12">
 	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
 		{#each videos as v}
+			{#if !(v.is_cn === false && $locale === 'cn')}
 			<div>
 				<div class="v-wrap">
-					<iframe src="https://www.youtube.com/embed/{v.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<!--					<iframe src="https://www.youtube.com/embed/{v.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>-->
+					<video preload="metadata" controls playsinline class="absolute inset-0 w-full h-full">
+						<source src={v.video_url} type="video/mp4">
+					</video>
 				</div>
 				<h2 class="text-p2 mt-2">{v[`title_${$locale}`]}</h2>
-				<p class="text-sm text-gray-400">{v[`author_${$locale}`]}</p>
+<!--				<p class="text-sm text-gray-400">{v[`author_${$locale}`]}</p>-->
 			</div>
+			{/if}
 		{/each}
 	</div>
 </div>
