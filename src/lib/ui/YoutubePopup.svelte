168472e7da3725bd65802643453ab0100b86fe79
cliff <cliff.wong@ehlacademy.org>
2024-01-29 10:54:09 +0800

    update version

--- src/lib/ui/YoutubePopup.svelte
+++ src/lib/ui/YoutubePopup.svelte
@@ -4,5 +4,5 @@
 
 <div class="relative -mx-2 -my-2">
 	<div style="padding-top: 56.25%"></div>
-	<iframe class="absolute inset-0 w-full h-full" src="https://www.youtube.com/embed/MODs6ghKO0A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<iframe class="absolute inset-0 w-full h-full" src="https://www.youtube.com/embed/{id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
